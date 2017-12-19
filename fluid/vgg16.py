269af30be8a9374953e85f5768b83a8f0f852d17
Yibing Liu <liuyibing01@baidu.com>
2017-12-19 11:42:24 +0000

    Update vgg16 in fluid

--- fluid/vgg16.py
+++ fluid/vgg16.py
@@ -11,17 +11,17 @@ import functools
 
 parser = argparse.ArgumentParser("VGG16 benchmark.")
 parser.add_argument(
-    '--batch_size', type=int, default=32, help="Batch size for training.")
+    '--batch_size', type=int, default=128, help="Batch size for training.")
 parser.add_argument(
     '--learning_rate',
     type=float,
     default=1e-3,
     help="Learning rate for training.")
-parser.add_argument('--num_passes', type=int, default=10, help="No. of passes.")
+parser.add_argument('--num_passes', type=int, default=50, help="No. of passes.")
 parser.add_argument(
     '--device',
     type=str,
-    default='CPU',
+    default='GPU',
     choices=['CPU', 'GPU'],
     help="The device type.")
 parser.add_argument(
@@ -30,14 +30,6 @@ parser.add_argument(
     default='NHWC',
     choices=['NCHW', 'NHWC'],
     help='The data order, now only support NCHW.')
-parser.add_argument(
-    '--num_skip_batch',
-    type=int,
-    default=0,
-    help='The first #num_skip_batch batches'
-    'will be skipped for timing.')
-parser.add_argument(
-    '--iterations', type=int, default=10, help='Maximum iterations')
 args = parser.parse_args()
 
 
@@ -64,13 +56,31 @@ def vgg16_bn_drop(input):
     fc1 = fluid.layers.fc(input=drop, size=512, act=None)
     bn = fluid.layers.batch_norm(input=fc1, act='relu')
     drop2 = fluid.layers.dropout(x=bn, dropout_prob=0.5)
-    fc2 = fluid.layers.fc(input=drop2, size=102, act=None)
+    fc2 = fluid.layers.fc(input=drop2, size=10, act=None)
     return fc2
 
 
+def eval_test(exe):
+    test_reader = paddle.batch(paddle.dataset.cifar.test10(), batch_size=100)
+    accuracy.reset(exe)
+    for batch_id, data in enumerate(test_reader()):
+        img_data = np.array(map(lambda x: x[0].reshape([3, 32, 32]),
+                                data)).astype(DTYPE)
+        y_data = np.array(map(lambda x: x[1], data)).astype("int64")
+        y_data = y_data.reshape([len(y_data), 1])
+
+        exe.run(framework.default_main_program(),
+                feed={"pixel": img_data,
+                      "label": y_data},
+                fetch_list=[avg_cost] + accuracy.metrics)
+
+    pass_acc = accuracy.eval(exe)
+    return pass_acc
+
+
 def main():
-    classdim = 102
-    data_shape = [3, 224, 224]
+    classdim = 10
+    data_shape = [3, 32, 32]
 
     images = fluid.layers.data(name='pixel', shape=data_shape, dtype='float32')
     label = fluid.layers.data(name='label', shape=[1], dtype='int64')
@@ -87,24 +97,22 @@ def main():
 
     train_reader = paddle.batch(
         paddle.reader.shuffle(
-            paddle.dataset.flowers.train(), buf_size=5120),
+            paddle.dataset.cifar.train10(), buf_size=5120),
         batch_size=args.batch_size)
+    test_reader = paddle.batch(paddle.dataset.cifar.test10(), batch_size=100)
 
     place = fluid.CPUPlace() if args.device == 'CPU' else fluid.GPUPlace(0)
     exe = fluid.Executor(place)
 
     exe.run(fluid.default_startup_program())
 
-    iters, num_samples, start_time = 0, 0, 0.0
+    iters = 0
     for pass_id in range(args.num_passes):
-        if args.iterations == iters:
-            break
+        # train
+        start_time = time.clock()
+        num_samples = 0
         accuracy.reset(exe)
-        pass_begin = time.clock()
         for batch_id, data in enumerate(train_reader()):
-            batch_begin = time.clock()
-            if args.num_skip_batch == iters:
-                start_time = time.clock()
             img_data = np.array(map(lambda x: x[0].reshape(data_shape),
                                     data)).astype("float32")
             y_data = np.array(map(lambda x: x[1], data)).astype("int64")
@@ -117,22 +125,29 @@ def main():
                                 feed={"pixel": img_data,
                                       "label": y_data},
                                 fetch_list=[avg_cost] + accuracy.metrics)
-            batch_end = time.clock()
-            print("pass=%d, batch=%d, loss=%f, acc=%f, time=%f" %
-                  (pass_id, batch_id, loss, acc, batch_end - batch_begin))
             iters += 1
-            if iters > args.num_skip_batch:
-                num_samples += len(data)
-            if args.iterations == iters:
-                break
-        pass_acc = accuracy.eval(exe)
-        pass_end = time.clock()
-        print("pass %d, training_acc=%f, elapsed_time=%fs\n" %
-              (pass_id, pass_acc, (pass_end - pass_begin)))
+            num_samples += len(data)
+            print("Pass = %d, Iters = %d, Loss = %f, Accuracy = %f" %
+                  (pass_id, iters, loss, acc))
 
-    duration = time.clock() - start_time
-    imgs_per_sec = num_samples / duration
-    print("duration=%fs, performance=%fimgs/s" % (duration, imgs_per_sec))
+        pass_elapsed = time.clock() - start_time
+        # test
+        accuracy.reset(exe)
+        for batch_id, data in enumerate(test_reader()):
+            img_data = np.array(map(lambda x: x[0].reshape(data_shape),
+                                    data)).astype("float32")
+            y_data = np.array(map(lambda x: x[1], data)).astype("int64")
+            y_data = y_data.reshape([len(y_data), 1])
+
+            exe.run(fluid.default_main_program(),
+                    feed={"pixel": img_data,
+                          "label": y_data},
+                    fetch_list=[avg_cost] + accuracy.metrics)
+
+        pass_acc = accuracy.eval(exe)
+        print(
+            "Pass = %d, Training performance = %f imgs/s, Test accuracy = %f\n"
+            % (pass_id, num_samples / pass_elapsed, pass_acc))
 
 
 def print_arguments():
