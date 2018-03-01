51ca97b5f0c05460e4e14284c9c723c301a2d413
qiaolongfei <qiaolongfei@baidu.com>
2018-03-01 17:21:16 +0800

    update install_dependency.sh

--- image_resnet/pytorch/install_dependency.sh
+++ image_resnet/pytorch/install_dependency.sh
@@ -1,2 +1,6 @@
-pip3 install http://download.pytorch.org/whl/cu80/torch-0.3.1-cp35-cp35m-linux_x86_64.whl 
-pip3 install torchvision
+
+pip install http://download.pytorch.org/whl/cu80/torch-0.3.1-cp27-cp27mu-linux_x86_64.whl 
+pip install torchvision 
+
+# if the above command does not work, then you have python 2.7 UCS2, use this command 
+#pip install http://download.pytorch.org/whl/cu80/torch-0.3.1-cp27-cp27m-linux_x86_64.whl
