daea2998533011223d3d80dc12aae5db6872e6ad
chengduoZH <zhaochengduo@163.com>
2018-03-28 16:01:00 +0800

    fix SE-ResNeXt-152_parallel_exe

--- fluid/SE-ResNeXt-152/train_parallel_executor.py
+++ fluid/SE-ResNeXt-152/train_parallel_executor.py
@@ -25,19 +25,13 @@ import paddle.fluid.profiler as profiler
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(
-        'SE-ResNeXt-152 parallel-executor profile.')
+    parser = argparse.ArgumentParser('SE-ResNeXt-152 parallel-executor model.')
     parser.add_argument(
         '--use_mem_opt',
         type=distutils.util.strtobool,
         default=True,
         help='use memory optimize')
     parser.add_argument('--per_gpu_batch_size', type=int, default=12, help='')
-    parser.add_argument(
-        '--total_batch_num',
-        type=int,
-        default=40,
-        help='total batch num for per_gpu_batch_size')
     parser.add_argument(
         '--number_iteration',
         type=int,
@@ -65,18 +59,12 @@ def conv_bn_layer(input, num_filters, filter_size, stride=1, groups=1,
         groups=groups,
         act=None,
         bias_attr=False)
-    return fluid.layers.batch_norm(input=conv, act=act, momentum=0.1)
+    return fluid.layers.batch_norm(input=conv, act=act)
 
 
 def squeeze_excitation(input, num_channels, reduction_ratio):
-    #pool = fluid.layers.pool2d(
-    #    input=input, pool_size=0, pool_type='avg', global_pooling=True)
-    conv = input
-    shape = conv.shape
-    reshape = fluid.layers.reshape(
-        x=conv, shape=[-1, shape[1], shape[2] * shape[3]])
-    pool = fluid.layers.reduce_mean(input=reshape, dim=2)
-
+    pool = fluid.layers.pool2d(
+        input=input, pool_size=0, pool_type='avg', global_pooling=True)
     squeeze = fluid.layers.fc(input=pool,
                               size=num_channels / reduction_ratio,
                               act='relu')
@@ -100,13 +88,11 @@ def shortcut(input, ch_out, stride):
 
 
 def bottleneck_block(input, num_filters, stride, cardinality, reduction_ratio):
-    # The number of first 1x1 convolutional channels for each bottleneck build block
-    # was halved to reduce the compution cost.
     conv0 = conv_bn_layer(
         input=input, num_filters=num_filters, filter_size=1, act='relu')
     conv1 = conv_bn_layer(
         input=conv0,
-        num_filters=num_filters * 2,
+        num_filters=num_filters,
         filter_size=3,
         stride=stride,
         groups=cardinality,
@@ -119,24 +105,47 @@ def bottleneck_block(input, num_filters, stride, cardinality, reduction_ratio):
         reduction_ratio=reduction_ratio)
 
     short = shortcut(input, num_filters * 2, stride)
-
     return fluid.layers.elementwise_add(x=short, y=scale, act='relu')
 
 
-def SE_ResNeXt152(input, class_dim):
-    cardinality = 64
-    reduction_ratio = 16
-    depth = [3, 8, 36, 3]
-    num_filters = [128, 256, 512, 1024]
+def SE_ResNeXt(input, class_dim, infer=False, layers=152):
+    supported_layers = [50, 152]
+    if layers not in supported_layers:
+        print("supported layers are", supported_layers, "but input layer is ",
+              layers)
+        exit()
+    if layers == 50:
+        cardinality = 32
+        reduction_ratio = 16
+        depth = [3, 4, 6, 3]
+        num_filters = [128, 256, 512, 1024]
 
-    conv = conv_bn_layer(
-        input=input, num_filters=64, filter_size=3, stride=2, act='relu')
-    conv = conv_bn_layer(
-        input=conv, num_filters=64, filter_size=3, stride=1, act='relu')
-    conv = conv_bn_layer(
-        input=conv, num_filters=128, filter_size=3, stride=1, act='relu')
-    conv = fluid.layers.pool2d(
-        input=conv, pool_size=3, pool_stride=2, pool_padding=1, pool_type='max')
+        conv = conv_bn_layer(
+            input=input, num_filters=64, filter_size=7, stride=2, act='relu')
+        conv = fluid.layers.pool2d(
+            input=conv,
+            pool_size=3,
+            pool_stride=2,
+            pool_padding=1,
+            pool_type='max')
+    elif layers == 152:
+        cardinality = 64
+        reduction_ratio = 16
+        depth = [3, 8, 36, 3]
+        num_filters = [128, 256, 512, 1024]
+
+        conv = conv_bn_layer(
+            input=input, num_filters=64, filter_size=3, stride=2, act='relu')
+        conv = conv_bn_layer(
+            input=conv, num_filters=64, filter_size=3, stride=1, act='relu')
+        conv = conv_bn_layer(
+            input=conv, num_filters=128, filter_size=3, stride=1, act='relu')
+        conv = fluid.layers.pool2d(
+            input=conv,
+            pool_size=3,
+            pool_stride=2,
+            pool_padding=1,
+            pool_type='max')
 
     for block in range(len(depth)):
         for i in range(depth[block]):
@@ -147,16 +156,13 @@ def SE_ResNeXt152(input, class_dim):
                 cardinality=cardinality,
                 reduction_ratio=reduction_ratio)
 
-    #pool = fluid.layers.pool2d(
-    #    input=conv, pool_size=0, pool_type='avg', global_pooling=True)
-    shape = conv.shape
-    reshape = fluid.layers.reshape(
-        x=conv, shape=[-1, shape[1], shape[2] * shape[3]])
-    pool = fluid.layers.reduce_mean(input=reshape, dim=2)
-    #yancanxiang: A drop out layer(with a drop ratio of 0.2) was inserted before the classifier layer.
-    dropout = fluid.layers.dropout(x=pool, dropout_prob=0.2)
-    # Classifier layer:
-    out = fluid.layers.fc(input=dropout, size=class_dim, act='softmax')
+    pool = fluid.layers.pool2d(
+        input=conv, pool_size=0, pool_type='avg', global_pooling=True)
+    if not infer:
+        drop = fluid.layers.dropout(x=pool, dropout_prob=0.2)
+    else:
+        drop = pool
+    out = fluid.layers.fc(input=drop, size=class_dim, act='softmax')
     return out
 
 
