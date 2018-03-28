bdf5ed1b81dfef35a086491f818ef03f89724687
dongzhihong <dzhwinter@gmail.com>
2018-03-29 00:29:32 +0800

    "add details"

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script benchmarking the PaddlePaddle Fluid on
 # single thread single GPU.
-
+export CUDNN_PATH=/paddle/cudnn_v5/cuda/lib
 
 # disable openmp and mkl parallel
 #https://github.com/PaddlePaddle/Paddle/issues/7199
@@ -23,11 +23,11 @@ fi
 # disable multi-gpu if have more than one
 export CUDA_VISIBLE_DEVICES=0
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/paddle/cudnn_v5/cuda/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$CUDNN_PATH:$LD_LIBRARY_PATH
 
 
 # vgg16
-# cifar10 gpu 128
+# cifar10 gpu cifar10 128
 FLAGS_benchmark=true python fluid/vgg16.py \
                --device=GPU \
                --batch_size=128 \
@@ -35,7 +35,8 @@ FLAGS_benchmark=true python fluid/vgg16.py \
                --iterations=30  \
                2>&1 > vgg16_gpu_128.log
 
-resnet50
+# resnet50
+# resnet50 gpu cifar10 128
 FLAGS_benchmark=true python fluid/resnet50.py \
                --device=GPU \
                --batch_size=128 \
@@ -44,3 +45,5 @@ FLAGS_benchmark=true python fluid/resnet50.py \
                --skip_batch_num=5 \
                --iterations=30 \
                2>&1 > resnet50_gpu_128.log
+
+# lstm
