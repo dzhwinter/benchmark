4b62852383982b29ae3b5e382c8158460c7ce9d9
dzhwinter <dzhwinter@gmail.com>
2018-03-29 01:07:31 -0700

    "fix ci"

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # This script benchmarking the PaddlePaddle Fluid on
 # single thread single GPU.
-export CUDNN_PATH=/paddle/cudnn_v5/cuda/lib
+
+#export FLAGS_fraction_of_gpu_memory_to_use=0.0
+export CUDNN_PATH=/paddle/cudnn_v5
 
 # disable openmp and mkl parallel
 #https://github.com/PaddlePaddle/Paddle/issues/7199
@@ -25,7 +27,6 @@ export CUDA_VISIBLE_DEVICES=0
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$CUDNN_PATH:$LD_LIBRARY_PATH
 
-#export FLAGS_fraction_of_gpu_memory_to_use=0.0
 # only query the gpu used
 nohup stdbuf -oL nvidia-smi \
       --id=${CUDA_VISIBLE_DEVICES} \
@@ -83,10 +84,10 @@ FLAGS_benchmark=true stdbuf -oL python fluid/resnet50.py \
                2>&1 | tee -a resnet50_gpu_flowers_64.log
 
 # lstm
-# lstm gpu imdb 128
+# lstm gpu imdb 32 # tensorflow only support batch=32
 FLAGS_benchmark=true stdbuf -oL python fluid/stacked_dynamic_lstm.py \
                --device=GPU \
-               --batch_size=32 \ # tensorflow only support batch=32
+               --batch_size=32 \
                --skip_batch_num=5 \
                --iterations=30 \
                --hidden_dim=512 \
